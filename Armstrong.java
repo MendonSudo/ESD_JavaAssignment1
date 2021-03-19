a55b741b38362cff1b24288b04d11126c183e106
MendonSudo <mmendon.mendon@gmail.com>
2021-03-19 10:13:49 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -3,14 +3,20 @@
 class Armstrong {
 	public static void main(String args[]) {
 		int num = 153;
-		int sum=0,temp,digit;
+		int sum=0,temp,digit,iter=0;
 		temp = num;
 		
+		while(temp !=0) {
+			iter = iter+1;
+			temp = temp / 10;
+			System.out.println(iter);
+		}
+		/*
 		while(num > 0) {
 			digit = num % 10;
-			sum += (digit * digit * digit);
+			sum += Math.pow(digit,num);
 			num = num / 10;
-		}
+		} */
 		if(sum == temp) {
 			System.out.println("Armstrong\n");
 		}
