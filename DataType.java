d4dc3fc06c404d60ea2b7fdb33d7b05f61ebccab
MendonSudo <mmendon.mendon@gmail.com>
2021-03-19 10:48:40 +0530

    Create DataType.java

---
+++ DataType.java
@@ -0,0 +1,20 @@
+/* 2. Implement a JAVA program to check the default values of the Datatypes. */
+
+class DataType {
+	static int a_int;
+	static float b_float;
+	static String c_string;
+	static double d_double;
+	static boolean e_boolean;
+	static byte f_byte;
+
+	public static void main(String args[]) {
+
+		System.out.println("int: "+a_int);
+		System.out.println("float: "+b_float);
+		System.out.println("String: "+c_string);
+		System.out.println("double: "+d_double);
+		System.out.println("boolean: "+e_boolean);
+		System.out.println("byte: "+f_byte);
+	}
+}
