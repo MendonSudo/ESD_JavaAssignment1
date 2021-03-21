a5a27b5dfa9eff0428ba1fb23cef83a49d84d153
MendonSudo <mmendon.mendon@gmail.com>
2021-03-21 09:54:07 +0530

    Create SquareNum.java

---
+++ SquareNum.java
@@ -0,0 +1,8 @@
+/*4. Implement a JAVA program to find the square of the number. */
+
+class SquareNum {
+	public static void main(String args[]) {
+		int num = 7;
+		System.out.println("Square of "+num+" is "+(num*num));
+	}
+}
