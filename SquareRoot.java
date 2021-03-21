46f8272ae93b2e8ee3f40d25bb8621cde29de51b
MendonSudo <mmendon.mendon@gmail.com>
2021-03-21 09:54:23 +0530

    Create SquareRoot.java

---
+++ SquareRoot.java
@@ -0,0 +1,8 @@
+/* 5. Implement a JAVA program to find the square root of the number. (import MATH,can use sqrt) */
+
+class SquareRoot {
+	public static void main(String args[]) {
+		int num = 63;
+		System.out.println("Square root of "+num+" is "+Math.sqrt(num));
+	}
+}
