9ff5dcffeae5b7027c0545eedb011e240cd8488b
MendonSudo <mmendon.mendon@gmail.com>
2021-03-30 17:47:20 +0530

    create

---
+++ FindDet.java
@@ -0,0 +1,19 @@
+/* 16. Find the determinant, for the quadratic equations. */
+
+import java.util.*;
+
+class FindDet {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of a: ");
+		double a = sc.nextDouble();
+		System.out.println("Enter the value of b: ");
+		double b = sc.nextDouble();
+		System.out.println("Enter the value of c: ");
+		double c = sc.nextDouble();
+		
+		double det = (b*b) - (4*a*c);
+		
+		System.out.println("Determinant of the equation is "+det);
+	}
+}
---
+++ FindEucDist.java
@@ -0,0 +1,19 @@
+/* 17. Find the dictance between two points, using euclidean distance. */
+
+import java.util.*;
+
+class FindEucDist {
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the coordinates of the 1st point");
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		System.out.println("Enter the coordinates of the 2nd point");
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		
+		double distance = Math.sqrt((y2-y1)*(y2-y1) + (x2-x1)*(x2-x1));
+		System.out.println("Distance is "+distance);
+	}
+}
