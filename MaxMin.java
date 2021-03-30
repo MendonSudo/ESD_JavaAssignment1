7754b354453916554a6de9e27832323e817c127c
MendonSudo <mmendon.mendon@gmail.com>
2021-03-30 16:51:04 +0530

    Create MaxMin.java

---
+++ MaxMin.java
@@ -0,0 +1,18 @@
+/* 13. Implement a JAVA program to find the max and min of given two numbers. */
+
+import java.util.*;
+
+class MaxMin {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter two numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a>b)
+			System.out.println(a +" is greater than "+b);
+		else
+			System.out.println(b +" is greater than "+a);
+	}
+}
